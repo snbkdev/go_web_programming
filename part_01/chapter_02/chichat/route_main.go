b02a0f7188f51d6547f2b5337c5a8fcfda510d57
Asanbek09 <samudinua@gmail.com>
2026-01-14 16:08:49 +0600

    Начало создание небольшого проекта

---
+++ part_01/chapter_02/chichat/route_main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	threads, err := data.Threads(); if err == nil {
+		_, err := session(w, r)
+		public_tmpl_files := []string{"templates/layout.html", "templates/public.navbar.html", "templates/index.html"}
+		private_tmpl_files := []string{"templates/layout.html", "templates/private.navbar.html", "templates/index.html"}
+
+		var templates *template.Template
+		if err != nil {
+			templates = template.Must(template.ParseFiles(private_tmpl_files...))
+		} else {
+			templates = template.Must(template.ParseFiles(public_tmpl_files...))
+		}
+
+		templates.ExecuteTemplate(w, "layout", threads)
+	}
+}
