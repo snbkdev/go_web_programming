0015b1e79c144cfb4b11458bd6156a0a1cdc0101
Asanbek09 <samudinua@gmail.com>
2026-01-26 11:27:36 +0600

    chapter 09 - примеры с channel

---
+++ part_03/chapter_09/waitgroups/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func printNumbers2(wg *sync.WaitGroup) {
+	for i := 0; i < 10; i++ {
+		time.Sleep(1 * time.Microsecond)
+		fmt.Printf("%d ",i )
+	}
+
+	wg.Done()
+}
+
+func printLetters2(wg *sync.WaitGroup) {
+	for i := 'A'; i < 'A' + 10; i++ {
+		time.Sleep(1 * time.Microsecond)
+		fmt.Printf("%c ", i)
+	}
+
+	wg.Done()
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go printLetters2(&wg)
+	go printNumbers2(&wg)
+	wg.Wait()
+}
